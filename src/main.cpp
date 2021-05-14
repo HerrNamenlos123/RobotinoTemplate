ca360df9e9cdbc027f6718242c3fcd225cc3e31d
HerrNamenlos123 <herrnamenlos123@gmail.com>
2021-05-14 17:00:14 +0200

    First reallife test

--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,15 @@
 int main() {
 
     // Initialize logging
-    LOG_INIT(LOG_LEVEL_DEBUG);
+    //LOG_INIT(LOG_LEVEL_DEBUG);
+    LOG_INIT(LOG_LEVEL_TRACE);
 
     {
         Robotino robotino;
-        robotino.Connect("127.0.0.1");
+        robotino.Connect("172.26.1.1");
 
         do {
-            robotino.DriveDirect(-0.2, 0, 0);
+            robotino.DriveDirect(0.2, 0, 0);
             std::this_thread::sleep_for(std::chrono::milliseconds(50));
         } while (!robotino.GetBumper());
 
