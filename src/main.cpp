4f9321b9b1b67c38e631340642fa928f85e14154
HerrNamenlos123 <herrnamenlos123@gmail.com>
2021-05-14 17:55:04 +0200

    Added Pose func

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,10 @@ int main() {
         robotino.Connect("172.26.1.1");
 
         do {
-            robotino.DriveDirect(0.2, 0, 0);
+            auto pose = robotino.GetPose();
+            LOG_INFO("X: {}, Y: {}, PHI: {}", pose[0], pose[1], pose[2]);
+
+            robotino.DriveDirect(0.2, 0.1, 3);
             std::this_thread::sleep_for(std::chrono::milliseconds(50));
         } while (!robotino.GetBumper());
 
