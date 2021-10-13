b24600da3645d1bd26a584e0c56986c08f5199e5
HerrNamenlos123 <herrnamenlos123@gmail.com>
2021-10-13 10:18:06 +0200

    Windows binaries linking

--- premake5.lua
+++ premake5.lua
@@ -49,6 +49,7 @@ project (projectName)
         targetdir (_SCRIPT_DIR .. "/bin/Deploy")
     filter {}
 
+    -- Include directories and library linking
     filter { "configurations:Debug", "system:windows" }
         includedirs { _SCRIPT_DIR .. "/include", "$(ROBOTINOLIB_INCLUDE_DIRECTORY)" }
         links { "$(ROBOTINOLIB_DEBUG_LINKS)" }
@@ -57,11 +58,28 @@ project (projectName)
         links { "$(ROBOTINOLIB_RELEASE_LINKS)" }
     filter { "configurations:Deploy", "system:windows" }
         includedirs { _SCRIPT_DIR .. "/include", "$(ROBOTINOLIB_INCLUDE_DIRECTORY)" }
-        links { "$(ROBOTINOLIB_RELEASE_LINKS)" }
+        links { "$(ROBOTINOLIB_DEPLOY_LINKS)" }
     filter { "system:not windows" }
         includedirs { _SCRIPT_DIR .. "/include", "/usr/local/include/" }
         libdirs "/usr/local/bin/"
         links { "rec_robotino_api2", "RobotinoLib" }
     filter {}
     
+    -- Main source files
     files { _SCRIPT_DIR .. "/include/**", _SCRIPT_DIR .. "/src/**" }
+    
+    -- RobotinoApi2 binaries (windows only)
+    filter "system:windows"
+        local source1 = "$(ROBOTINOLIB_BINARY_DIR)/rec_robotino_api2.dll"
+        local target = _SCRIPT_DIR .. "/bin/%{cfg.buildcfg}"
+        
+        local source2 = "$(ROBOTINOLIB_BINARY_DIR)/rec_robotino_rpc.dll"
+        
+        local source3 = "$(ROBOTINOLIB_BINARY_DIR)/rec_rpc.dll"
+
+        postbuildcommands { 
+            "IF NOT EXIST \"" .. target .. "/rec_robotino_api2.dll\" xcopy /b /y /f /i /v \"" .. source1 .. "\" \"" .. target .. "\" && echo " .. source1 .. " -> " .. target .. "/rec_robotino_api2.dll",
+            "IF NOT EXIST \"" .. target .. "/rec_robotino_rpc.dll\" xcopy /b /y /f /i /v \"" .. source2 .. "\" \"" .. target .. "\" && echo " .. source2 .. " -> " .. target .. "/rec_robotino_rpc.dll",
+            "IF NOT EXIST \"" .. target .. "/rec_rpc.dll\" xcopy /b /y /f /i /v \"" .. source3 .. "\" \"" .. target .. "\" && echo " .. source3 .. " -> " .. target .. "/rec_rpc.dll"
+        }
+    filter {}
