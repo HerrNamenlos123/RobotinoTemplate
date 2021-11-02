75ec17169952c8d6e3968fde4fe519804379b241
HerrNamenlos123 <herrnamenlos123@gmail.com>
2021-11-02 18:16:51 +0100

    Cleaned template

--- src/RobotinoAction.cpp
+++ src/RobotinoAction.cpp
@@ -1,33 +1,15 @@
 
 #include "Robotino.h"
 
-#include <sstream>
-#include <fstream>
-#include <iostream>
-
 void RobotinoAction() {
 	Robotino::Robotino robotino;
-    robotino.Connect("172.26.1.1");
+    robotino.Connect("127.0.0.1");
 
     while (!robotino.GetBumper()) {
-        robotino.DriveDirect(0.0f, 0.1f, 0.0f);
-        //std::this_thread::sleep_for(std::chrono::milliseconds(50));
+        robotino.DriveDirect(0.1f, 0.0f, 0.0f);
+        LOG_DEBUG("Driving forward...");
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
-
-    //do {
-    //    //auto pose = robotino.GetPose();
-    //    //LOG_DEBUG("X: {}, Y: {}, PHI: {}", pose[0], pose[1], pose[2]);
-    //    //robotino.GetCamera();
-    //    //Robotino::Image image = robotino.GetCamera();
-	//	
-	//	/*std::ofstream myfile;
-	//	myfile.open("image.raw", std::ios::binary);
-	//	myfile.write((char*)image.raw(), image.width * image.height * 3);
-	//	myfile.close();*/
-    //
-    //    robotino.DriveDirect(0.2f, 0.1f, 3.f);
-    //    std::this_thread::sleep_for(std::chrono::milliseconds(50));
-    //} while (!robotino.GetBumper());
 
     robotino.Disconnect();
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,29 @@
 
-#include <iostream>
 #include "Robotino.h"
 #include "RobotinoAction.h"
-#include "Robotino/RobotinoApi2.h"
 
 #pragma warning( disable : 4101 )	// Suppress warning about unused variable 'e' (Only in this .cpp file)
 
 int main() {
 
     try {
+        LOG_TRACE("Starting RobotinoAction...");
+
         RobotinoAction();
+
+        LOG_INFO("Application stopped");
     }
     catch (const Robotino::NotConnectedException& e) {
         LOG_CRITICAL("Robotino is not connected!");
-        rec::robotino::api2::shutdown();
-        return 0;
     }
     catch (const std::exception& e) {
         LOG_CRITICAL("std::exception: {}", e.what());
-        rec::robotino::api2::shutdown();
-        return 0;
     }
     catch (...) {
         LOG_CRITICAL("Unknown exception!");
-        rec::robotino::api2::shutdown();
-        return 0;
     }
 
-    rec::robotino::api2::shutdown();
+    Robotino::Shutdown();
 
-    LOG_INFO("Application stopped");
+    return 0;
 }
